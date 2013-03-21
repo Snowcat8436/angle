d33ba24b5708d08063fded824393df997480cc7f
Yongkyu Lee <yongku1004@naver.com>
2013-03-21 23:29:21 +0900

    3/21
    3월 21일치!

--- Tween-coffec.js
+++ Tween-coffec.js
@@ -270,3 +270,66 @@ TWEEN.Easing =
 			if k < 0.5 then return TWEEN.Easing.Bounce.In( k * 2 ) * 0.5 else return TWEEN.Easing.Bounce.Out( k * 2 - 1 ) * 0.5 + 0.5
 
 TWEEN.Interpolation = 
+	Linear: (v,k) ->
+		n = v.length() -1
+		f =  m * k
+		i = Math.floor f
+		fn=TWEEN.Interpolation.Utils.Linear
+
+		if k < 0 then return fn v[ 0 ], v[ 1 ], f 
+		if k > 1 then return fn v[ m ], v[ m - 1 ], m - f 
+
+		return fn v[ i ], v[ i + 1 > m ? m : i + 1 ], f - i
+
+	Bezier: (v, k) ->
+		b = 0
+		n = v.length - 1
+		pw = Math.pow
+		bn = TWEEN.Interpolation.Utils.Bernstein
+
+		for i in [0...n+1]
+			b += pw (1 - k, n - i)  * pw (k, i)  * v[ i ] * bn(n, i)
+
+		return b
+
+	CatmullRom: ( v, k ) ->
+
+		m = v.length - 1
+		f = m * k
+		i = Math.floor f
+		fn = TWEEN.Interpolation.Utils.CatmullRom
+
+		if v[ 0 ] is v[ m ]
+			if  k < 0  then i = Math.floor( f = m * ( 1 + k ) )
+			return fn v[ ( i - 1 + m ) % m ], v[ i ], v[ ( i + 1 ) % m ], v[ ( i + 2 ) % m ], f - i
+		else
+			if k < 0 then return v[ 0 ] - ( fn( v[ 0 ], v[ 0 ], v[ 1 ], v[ 1 ], -f ) - v[ 0 ] )
+			if k > 1 then return v[ m ] - ( fn( v[ m ], v[ m ], v[ m - 1 ], v[ m - 1 ], f - m ) - v[ m ] )
+
+			return fn( v[ i ? i - 1 : 0 ], v[ i ], v[ m < i + 1 ? m : i + 1 ], v[ m < i + 2 ? m : i + 2 ], f - i )
+	Utils:
+		Linear: ( p0, p1, t ) ->
+			return ( p1 - p0 ) * t + p0
+
+		Bernstein: ( n , i ) ->
+			fc = TWEEN.Interpolation.Utils.Factorial
+			return fc( n ) / fc( i ) / fc( n - i )
+
+		Factorial: ( () ->
+			a = [ 1 ]
+			return ( n ) ->
+				s = 1
+				if a[n] then return a[n];
+				for i in [n...1]
+					s *= i;
+				return a[ n ] = s;
+		)(), #??
+
+		CatmullRom: ( p0, p1, p2, p3, t ) ->
+			v0 = ( p2 - p0 ) * 0.5
+			v1 = ( p3 - p1 ) * 0.5
+			t2 = t * t
+			t3 = t * t2
+			return ( 2 * p1 - 2 * p2 + v0 + v1 ) * t3 + ( - 3 * p1 + 3 * p2 - 2 * v0 - v1 ) * t2 + v0 * t + p1
+
+
